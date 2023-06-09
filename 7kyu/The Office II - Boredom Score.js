6b5704ba182fd3ea23eff64a4075796091cb3ce9
c-pavi <c.pavishan@gmail.com>
2023-06-08 20:25:37 -0400

    7kyu June 8 2023

---
+++ 7kyu/The Office II - Boredom Score.js
@@ -0,0 +1,65 @@
+// Every now and then people in the office moves teams or departments. Depending what people are doing with their time they can become more or less boring. Time to assess the current team.
+
+// You will be provided with an object(staff) containing the staff names as keys, and the department they work in as values.
+
+// Each department has a different boredom assessment score, as follows:
+
+// accounts = 1
+// finance = 2
+// canteen = 10
+// regulation = 3
+// trading = 6
+// change = 6
+// IS = 8
+// retail = 5
+// cleaning = 4
+// pissing about = 25
+
+// Depending on the cumulative score of the team, return the appropriate sentiment:
+
+// <=80: 'kill me now'
+// < 100 & > 80: 'i can handle this'
+// 100 or over: 'party time!!'
+
+//my solutioon
+
+function boredom(staff){
+    let total = 0
+    let vals = {
+      'change' : 6,
+      'accounts' : 1, 
+      'finance' : 2,
+      'canteen' : 10,
+      'regulation' : 3,
+      'trading' : 6,
+      'IS' : 8,
+      'retail' : 5,
+      'cleaning' : 4,
+      'pissing about' : 25
+    }
+  
+    for (let item in staff) {
+    total += vals[`${staff[item]}`]
+  }
+    return total >= 100 ? 'party time!!' : total <= 80 ? 'kill me now' :  'i can handle this';
+  }
+
+// best solutiojn
+
+function boredom(staff) {
+    var teams = { 'accounts'      : 1,
+                  'finance'       : 2 ,
+                  'canteen'       : 10, 
+                  'regulation'    : 3, 
+                  'trading'      : 6, 
+                  'change'        : 6,
+                  'IS'            : 8,
+                  'retail'        : 5,
+                  'cleaning'      : 4,
+                  'pissing about' : 25},
+        score = Object.keys(staff)
+                      .map(key=>teams[staff[key]])
+                      .reduce((a,b) => a+b, 0);
+                      
+  return score <= 80 ? 'kill me now' : (score > 100 ? 'party time!!' : 'i can handle this');            
+  }
