32a6b8d6d0bff257a2494091ea255ea030e9feb1
c-pavi <c.pavishan@gmail.com>
2023-09-29 18:40:22 -0400

    Spet 29 1 7kyu

---
+++ 7kyu/Remove All The Marked Elements of a List.js
@@ -0,0 +1,25 @@
+// Define a method/function that removes from a given array of integers all the values contained in a second array.
+// Examples (input -> output):
+
+// * [1, 1, 2, 3, 1, 2, 3, 4], [1, 3] -> [2, 2, 4]
+// * [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2] -> [5, 6, 7, 8]
+// * [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 3] -> [8, 7, 6, 5, 1]
+
+// Enjoy it!!
+
+
+//my solution
+Array.prototype.remove_ = function(integer_list, values_list){
+    for(let i = 0; i < values_list.length ; i ++){
+     integer_list = integer_list.filter(item => item != values_list[i])
+    }
+    return integer_list
+  }
+
+//best solution
+
+Array.prototype.remove_ = function(integer_list, values_list){
+	return integer_list.filter(function (element) {
+  	return values_list.indexOf(element) === -1;
+  });
+}
